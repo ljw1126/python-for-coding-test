56cebe97447741fc0a97fd06131d81a3ad95c5aa
leejinwoo <leejinwoo1126@gmail.com>
2021-08-20 23:31:38 +0900

    백준 그리디 2문제 품

--- BOJ/1_입출력과사칙연산.py
+++ BOJ/1_입출력과사칙연산.py
@@ -13,11 +13,11 @@
 
 """
 
-# n = int(input())
+n = int(input())
 
-# for i in range(n):
-#      a,b = map(int,input().split())
-#      print(f"Case #{i+1}:{a}+{b}={a+b}")
+for i in range(n):
+     a,b = map(int,input().split())
+     print(f"Case #{i+1}:{a}+{b}={a+b}")
 
 # n = int(input())
 # array = []
